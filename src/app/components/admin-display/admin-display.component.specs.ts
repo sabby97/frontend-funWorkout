b686e70a9e93bdae5353b0acae11770f2f389ec4
mattramrev <matt.ramirez@revature.net>
2021-11-09 13:57:47 -0500

    adding admin display front end Matt

---
+++ src/app/components/admin-display/admin-display.component.specs.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AdminDisplayComponent } from './admin-display.component';
+
+describe('AdminDisplayComponent', () => {
+  let component: AdminDisplayComponent;
+  let fixture: ComponentFixture<AdminDisplayComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AdminDisplayComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AdminDisplayComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
